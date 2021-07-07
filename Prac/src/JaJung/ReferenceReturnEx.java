3d1cbe7f4a5868061ed464d15b82d140ff119368
Hong <fheldgktpdy@khu.ac.kr>
2021-07-07 18:55:01 +0900

    JaJung

---
+++ Prac/src/JaJung/ReferenceReturnEx.java
@@ -0,0 +1,19 @@
+package JaJung;
+
+class Data{int x;}
+
+public class ReferenceReturnEx {
+
+	public static void main(String[] args) {
+		Data d = new Data();
+		d.x = 10;
+		
+		Data d2 = copy(d);
+	}
+	
+	static Data copy(Data d){
+		Data tmp = new Data();
+		tmp.x = d.x;
+		return tmp;
+	}
+}
