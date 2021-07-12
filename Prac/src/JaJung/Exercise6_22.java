a7056f3248a3003b003546be81e6a39566cdad3b
Hong <fheldgktpdy@khu.ac.kr>
2021-07-13 01:05:56 +0900

    JaJung

---
+++ Prac/src/JaJung/Exercise6_22.java
@@ -0,0 +1,24 @@
+package JaJung;
+
+public class Exercise6_22 {
+	
+	static boolean isNumber(String str){
+		if(str==null || str=="")
+			return false;
+		for(int i=0; i<str.length(); i++){
+			char k = str.charAt(i);
+			if(k<'0'||k>'9')
+				return false;
+		}
+		return true;
+	}
+
+	public static void main(String[] args) {
+		String str = "123";
+		System.out.println(str+" 는 숫자입니까? "+isNumber(str)); 
+		str = "1234o";
+		System.out.println(str+" 는 숫자입니까? "+isNumber(str)); 
+
+	}
+
+}
